cc78dcc6a1d1a182e38fd569eec273d88296087a
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2022-08-09 14:53:21 +0100

    completing the variables file

---
+++ variables/3_string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	str := "hello i am here"
+
+	fmt.Println(str)
+}
---
+++ variables/4_bool.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	str1 := "i am here"
+	str2 := "he is here"
+	out := str1 == str2
+
+	fmt.Println(out)
+}
