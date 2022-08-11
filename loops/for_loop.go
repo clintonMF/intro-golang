dbc31248eb30a665fed79ba0ca4a6d99b8255565
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2022-08-11 15:20:21 +0100

    adding other types of loops

--- loops/for_loop.go
+++ loops/for_loop.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
+// there are various types of for loops in golang
+//
 func main() {
-	for i := 0; i < 7; i++ {
-		fmt.Println("clinton")
+	//for loops with condition
+	a := 0
+	for a < 5 {
+		fmt.Println("a")
+		a = a + 1
 	}
+
+	//for loops with initialisation, condition and update
+	for i := 0; i < 5; i++ {
+		fmt.Println("i")
+	}
+
+	// for loops over range
+	b := [6]int{1, 3, 4, 5, 6, 7}
+	for j, x := range b {
+		fmt.Printf("b[%d] = %d\n", j, x)
+	}
+
 }
