761a6b9a8ef89fc5fc44a1277c55b961355f54a9
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2022-08-23 22:23:29 +0100

    adding gitignore file and other minor changes

--- functions.go
+++ functions.go
@@ -2,15 +2,27 @@ package main
 
 import "fmt"
 
-func multi(a, b int) int {
+func multiply(a, b int) int {
 	out := a * b
 	return out
+}
+
+//the function below returns multiple values
+func add_multi(a, b int) (int, int) {
+	add := a + b
+	multi := a * b
+	return add, multi
 }
 
 func main() {
 	a := 3
 	b := 2
-	c := multi(a, b)
+	c := multiply(a, b)
 
 	fmt.Println(c)
+
+	var d, e int = add_multi(a, b)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Printf("addition = %d \nmultiplication = %d", d, e)
 }
